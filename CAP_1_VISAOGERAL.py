37d697f672feab6fe08c2c02ebeb9cf8458168ed
Sávio Trindade <122768191+savio-810@users.noreply.github.com>
2024-12-26 19:46:22 -0300

    6

--- CAP_1_VISAOGERAL.py
+++ CAP_1_VISAOGERAL.py
@@ -281,9 +281,9 @@ with st.container(border=True):
         st.metric(label="Total", value=f"{wpp_members.shape[0]}")
         st.metric(label="Conversão", value=f"{round(wpp_members.shape[0]/DF_CENTRAL_CAPTURA.shape[0] * 100, 2)}%", delta="")
     
-    with col_cpl:
-        st.subheader("CPL")
-        st.metric(label = "CPL Geral", value = f"{round(DF_PTRAFEGO_META_ADS["VALOR USADO"].sum()/DF_CENTRAL_CAPTURA.shape[0],2)}")
+    #with col_cpl:
+        #st.subheader("CPL")
+        #st.metric(label = "CPL Geral", value = f"{round(DF_PTRAFEGO_META_ADS["VALOR USADO"].sum()/DF_CENTRAL_CAPTURA.shape[0],2)}")
         #st.metric(label = "CPL Trafego", value = f"{round(DF_PTRAFEGO_META_ADS["VALOR USADO"].sum()/DF_CENTRAL_CAPTURA[DF_CENTRAL_CAPTURA["CAP UTM_MEDIUM"] == "pago"].shape[0],2)}")
 
 #------------------------------------------------------------
