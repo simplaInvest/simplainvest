81b22fa7954ba7d38f72997a36d51b022d4ebd2b
mil4zzo <working.milazzo@gmail.com>
2024-12-31 10:57:56 -0300

    Removido parametro 'format=' das formatações de datas do 'data_formatter.py'

--- libs/data_formatter.py
+++ libs/data_formatter.py
@@ -3,7 +3,7 @@ import pandas as pd
 def format_central_captura(df_central_captura):
     if df_central_captura.empty:
         raise ValueError("O DataFrame 'df_central_captura' está vazio.")
-    df_central_captura['CAP DATA_CAPTURA'] = pd.to_datetime(df_central_captura['CAP DATA_CAPTURA'], dayfirst=True, format="%m/%d/%Y %H:%M:%S").dt.date
+    df_central_captura['CAP DATA_CAPTURA'] = pd.to_datetime(df_central_captura['CAP DATA_CAPTURA'], dayfirst=True).dt.date
     return df_central_captura
 
 def format_grupos_wpp(df_grupos_wpp):
