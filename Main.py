bd370db69deca7deef9b8fc3f4235bbea27a8c0b
Sávio Trindade <122768191+savio-810@users.noreply.github.com>
2025-04-14 20:23:01 -0300

    hotfix 2

--- Main.py
+++ Main.py
@@ -49,31 +49,31 @@ else:
                             st.session_state["LANÇAMENTO"] = f"SW{VERSAO_PRINCIPAL}"
                 st.rerun()
 
-pages_after_load = {
-    'INÍCIO': [
-        st.Page("dummy_snippets.py", title="🏠 Início"),
-    ],
-    'CAPTAÇÃO': [
-        st.Page("CAP_1_VISAOGERAL.py", title="📊 Visão Geral"),
-        st.Page("CAP_2_PTRAFEGO.py", title="💲 Pesquisa de Tráfego"),
-        st.Page("CAP_3_PCOPY.py", title="🧑 Pesquisa de Copy"),
-        st.Page("CAP_4_GRUPOS_WPP.py", title="📞 Grupos de Whatsapp"),
-        st.Page("CAP_5_ANUNCIOS.py", title="📲 Anúncios"),
-        st.Page("5_General_Message_analyzer.py", title="💬 Message Analyser")
-    ],
-    'PRÉ-MATRÍCULA': [
-        st.Page("PM_1_VISAOGERAL.py", title="⭐ Visão Geral"),
-    ],
-    'VENDAS': [
-        st.Page("VENDAS_1_VISAOGERAL.py", title="💵 Visão geral"),
-    ],
-    'GERAL': [
-        st.Page("ETC_EVOLUCAO.py", title="🎯 Comparação de Lançamentos"),
-        st.Page("dbf_gen.py", title="🔮 Gerador de Debriefing")
-    ]
-}
+    pages_after_load = {
+        'INÍCIO': [
+            st.Page("dummy_snippets.py", title="🏠 Início"),
+        ],
+        'CAPTAÇÃO': [
+            st.Page("CAP_1_VISAOGERAL.py", title="📊 Visão Geral"),
+            st.Page("CAP_2_PTRAFEGO.py", title="💲 Pesquisa de Tráfego"),
+            st.Page("CAP_3_PCOPY.py", title="🧑 Pesquisa de Copy"),
+            st.Page("CAP_4_GRUPOS_WPP.py", title="📞 Grupos de Whatsapp"),
+            st.Page("CAP_5_ANUNCIOS.py", title="📲 Anúncios"),
+            st.Page("5_General_Message_analyzer.py", title="💬 Message Analyser")
+        ],
+        'PRÉ-MATRÍCULA': [
+            st.Page("PM_1_VISAOGERAL.py", title="⭐ Visão Geral"),
+        ],
+        'VENDAS': [
+            st.Page("VENDAS_1_VISAOGERAL.py", title="💵 Visão geral"),
+        ],
+        'GERAL': [
+            st.Page("ETC_EVOLUCAO.py", title="🎯 Comparação de Lançamentos"),
+            st.Page("dbf_gen.py", title="🔮 Gerador de Debriefing")
+        ]
+    }
 
-pages = pages_after_load
+    pages = pages_after_load
 
 
 
